46968bbe5c537fa24493b6969e18398b6ce95f68
Friosn <franz.rios.nino@gmail.com>
2022-11-11 14:13:07 +0100

    Footer

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
+import FooterStyled from '../ui/FooterStyled';
 
 const Footer = () => {
   return (
-    <div>
-      <p color="black" padding="10px">
-        Heeeeeeey! I Am A Footer
-      </p>
-    </div>
+    <FooterStyled>
+      <p>Developed by Franz Ríos Niño</p>
+      <div className="icons">
+        <a href="https://github.com/Friosn" target="_blank" rel="noreferrer">
+          <img
+            src="https://res.cloudinary.com/dfrmvbvdc/image/upload/v1668169251/Icon-set/Social/svg_xml_base64_PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGFyaWEtaGlkZGVuPSJ0cnVlIiBoZWlnaHQ9IjI0IiB2aWV3Qm94PSIwIDAgMTYgMTYiIHZlcnNpb249IjEuMSIgd2lkdGg9IjI0IiBkYXRhLXZpZXctY29tcG9uZW50PSJ0cnVlIiBjbGFzcz0i_jnt6oi.svg"
+            alt="GitHub"
+            id="git"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/francisco-r%C3%ADos-ni%C3%B1o/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://res.cloudinary.com/dfrmvbvdc/image/upload/v1668169370/Icon-set/Social/svg_xml_base64_PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxOSAxOCIgYXJpYS1oaWRkZW49InRydWUiIGNsYXNzPSJkLWJsb2NrIiBoZWlnaHQ9IjE4Ij48cGF0aCBkPSJNMy45NCAyQTIgMiAwIDEgMSAyIDBhMiAyIDAgMCAxIDEu_bbibxq.svg"
+            alt="Linkedin"
+            id="Linkedin"
+          />
+        </a>
+        <a href="https://twitter.com/Frriosn" target="_blank" rel="noreferrer">
+          <img
+            src="https://res.cloudinary.com/dfrmvbvdc/image/upload/v1668169251/Icon-set/Social/svg_xml_base64_PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNzMuNSAyMjIuMyIgaGVpZ2h0PSIxNiIgd2lkdGg9IjE2Ij48dGl0bGU_VHdpdHRlcjwvdGl0bGU_PHBhdGggZD0iTTI3My41IDI2LjNhMTA5Ljc3IDEwOS43NyAwIDAg_fsyht1.svg"
+            alt="Twitter"
+            id="Twitter"
+          />
+        </a>
+      </div>
+    </FooterStyled>
   );
 };
 
--- app/src/ui/FooterStyled.jsx
+++ app/src/ui/FooterStyled.jsx
@@ -3,10 +3,32 @@ import styled from 'styled-components';
 const FooterStyled = styled.footer`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   width: 100%;
-  height: 65px;
-  gap: 1.5rem;
-  padding: 3rem;
+  bottom: 0px;
+  color: black;
+  align-items: center;
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(210, 210, 230, 1) 100%
+  );
+
+  #git,
+  #Linkedin,
+  #Twitter {
+    margin: 0 1rem;
+    width: 1.6rem;
+    height: 1.6rem;
+    border-radius: 20%;
+
+    &:hover {
+      background-color: #f2723a;
+      transition: 1s ease-in-out;
+      transition-timing-function: 1s;
+    }
+  }
 `;
 
 export default FooterStyled;
