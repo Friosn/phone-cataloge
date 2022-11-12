7df4bd28e5cd67422ec0cdedf2992996510a35aa
Friosn <franz.rios.nino@gmail.com>
2022-11-12 20:14:34 +0100

    NotFound Page

--- app/src/pages/404.jsx
+++ app/src/pages/404.jsx
@@ -1,7 +1,27 @@
+import './404.css';
+
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import GalleryStyled from '../ui/GalleryStyled';
+import H2Styled from '../ui/H2Styled';
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <GalleryStyled className="notFoundStyle">
+      <div className="notFound">
+        <h1>Error 404</h1>
+        <H2Styled>Page Not Found</H2Styled>
+        <p>
+          We could not find the page you were looking for; sorry for the inconvinience
+        </p>
+        <p>
+          Try going back to our <Link to="">Home Page</Link> or to our{' '}
+          <Link to="phones">Gallery</Link>
+        </p>
+      </div>
+    </GalleryStyled>
+  );
 };
 
 export default NotFound;
