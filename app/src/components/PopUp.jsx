f0b99426322cb5563eb04faf1c92f25830ccaa19
Friosn <franz.rios.nino@gmail.com>
2022-11-13 22:33:36 +0100

    Problems with useReducer; popUp bug

--- app/src/components/PopUp.jsx
+++ app/src/components/PopUp.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
 import './Popup.css';
 
-export const Popup = ({ text, closePopup }) => {
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+export const Popup = ({ closePopup }) => {
+  const [phones, setPhones] = useState([]);
+
+  useEffect(() => {
+    const getPhones = async () => {
+      const rawData = await fetch('http://localhost:3000/phones');
+      const dataToJson = await rawData.json();
+      setPhones(dataToJson);
+    };
+    getPhones();
+  }, []);
+
   return (
     <div className="popup-container">
       <div className="popup-body">
-        <h1>{text}</h1>
+        {phones.length &&
+          phones.map((phone) => (
+            <div key={phone.id.target} className="popup_card">
+              <img src={phone.image} alt={phone.name} />
+              <h3>{phone.name}</h3>
+              <h4>{phone.manufacturer}</h4>
+              <p>{phone.description}</p>
+              <p>{phone.price}</p>
+              <p>{phone.screen}</p>
+              <p>{phone.processor}</p>
+              <p>{phone.ram}</p>
+            </div>
+          ))}
         <button onClick={closePopup}>Close X</button>
       </div>
     </div>
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import { Link } from 'react-router-dom';
 
 import Carousel from '../components/Carousel';
 import { Popup } from '../components/PopUp';
 /* import StalkBuild from '../ui/StalkyBtn'; */
+/* const initialState = () => ({});
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}; */
 const Home = () => {
+  /* const [state, dispatch] = useReducer(reducer, {}); */
   const [phones, setPhones] = useState([]);
   const [open, setOpen] = useState(false);
 
@@ -27,10 +39,8 @@ const Home = () => {
             <div key={phone.id} className="carousel_element">
               <h2>{phone.name}</h2>
               <img src={phone.image} alt={phone.name} />
-              <button onClick={() => setOpen(true)}> Heyy! Finally</button>
-              {open ? (
-                <Popup text="Fuck this :)" closePopup={() => setOpen(false)} />
-              ) : null}
+              <button onClick={() => setOpen(true)}> Detail</button>
+              {open ? <Popup closePopup={() => setOpen(false)} /> : null}
             </div>
           ))}
       </Carousel>
